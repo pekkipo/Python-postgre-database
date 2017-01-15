355c663b9f07119f81384c35ed58dabf72bf395b
pekkipo <pekkipodev@gmail.com>
2017-01-15 21:25:11 +0100

    ConnectionPool class

--- project/database.py
+++ project/database.py
@@ -8,7 +8,31 @@ connection_pool = pool.SimpleConnectionPool(1,
                                             database='Learning',
                                             host='localhost'
                                             )
-#mincon = how many connections should be created once the pool is created. here 1
-#maxcon = max number of connections. here 10
-
 # if no connections available, then it is gonna create 1
+# mincon = how many connections should be created once the pool is created. here 1
+# maxcon = max number of connections. here 10
+
+# this class is to teach the usage of WITH statement
+
+class ConnectionFromPool:
+    def __init__(self):
+        self.connection = None
+
+
+    # Makes possible to use WITH (needs enter and exit)
+    # init method is called before enter method
+    def __enter__(self):
+        # enter method is the beginning of the with statement
+        self.connection = connection_pool.getconn()
+        return self.connection
+        # thus this connection from the pool goes into connection variable in user.py when called using with statement
+    # gets and returns a new connection. Now this works in user.py 'with ConnectionPool() as connection:'
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit() # DON'T forget! Commit then close
+        # here we should be returning the connections
+        connection_pool.putconn(self.connection)
+        # putting the connection back
+# access
+# cp = ConnectionPool()
+# cp.connection_pool
--- project/user.py
+++ project/user.py
@@ -1,4 +1,4 @@
-from database import connection_pool
+from database import ConnectionFromPool
 # database is database.py file, connect is the method
 
 class User:
@@ -15,38 +15,38 @@ class User:
 
         # the way to do it without commits and closes below:
     def save_to_db(self):
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-             cursor.execute('INSERT INTO users(email,first_name,last_name) VALUES (%s,%s,%s)',
-                               (self.email, self.first_name, self.last_name))
-            # commit and close is done for me automatically
-        connection_pool.putconn(connection)
-        # putting the connection back
+        with ConnectionFromPool() as connection:
+            with connection.cursor() as cursor:
+                 cursor.execute('INSERT INTO users(email,first_name,last_name) VALUES (%s,%s,%s)',
+                                   (self.email, self.first_name, self.last_name))
+                # commit and close is done for me automatically
 
-        # Loading from postgres
 
-        # we ll be using classmethod!
-        # cls instead of self as an arg, so don't need to create object first later
-        # in usual class self is the currently bound object
-        # cls in calssmethod is currently bound class! User in this case
+
+    # Loading from postgres
+
+    # we ll be using classmethod!
+    # cls instead of self as an arg, so don't need to create object first later
+    # in usual class self is the currently bound object
+    # cls in calssmethod is currently bound class! User in this case
     @classmethod
     def load_from_db_by_email(cls, email): # cls - currently bound class. self woult be currently bound object
         # note that we must pass the email as a parameter
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
-                # execute(query, args for query)
-                # (email,) is kinda a tuple here
-                # tuple is immutable list, cannot change it
-                # that is why comma! to tell Python
-                # that it is a tuple
+        with ConnectionFromPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
+                    # execute(query, args for query)
+                    # (email,) is kinda a tuple here
+                    # tuple is immutable list, cannot change it
+                    # that is why comma! to tell Python
+                    # that it is a tuple
 
-                # cursor stores the result of the query
-            user_data = cursor.fetchone()
+                    # cursor stores the result of the query
+                user_data = cursor.fetchone()
 
-                # assuming that one row only mathces
-            return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
-                # call the User class and pass data into it
-                # if without var names - then be sure about the right order
-        #connection_pool.putconn(connection)
-        # it is after the return
+                    # assuming that one row only mathces
+                return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
+                    # call the User class and pass data into it
+                    # if without var names - then be sure about the right order
+
+            # it is after the return
