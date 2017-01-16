a0f6a7cbcebe4ff7f8c8882deeadf5470a52202f
pekkipo <pekkipodev@gmail.com>
2017-01-16 14:02:55 +0300

    Database class. Private var. **kwargs

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
+from database import Database
 from user import User
-
 #my_user = User('testuser@testuser.ru', 'Jose', 'Salvatierra',None)
 # None because postgre sql will give incremental value for id later
 
@@ -7,10 +7,17 @@ from user import User
 # basically that means User.save_to_db(my_user)
 
 
-my_user = User.load_from_db_by_email('testuser@testuser.ru')
+#my_user = User.load_from_db_by_email('testuser@testuser.ru')
 
 # test adding user again
 #my_user2 = User('testuser2@testuser2.ru', 'Jose2', 'Salvatierra2',None)
 #my_user2.save_to_db()
 
-print(my_user)
+#print(my_user)
+
+Database.initialize(user='postgres',
+                    password='1234',
+                    database='Learning',
+                    host='localhost')
+new_user = User('testuser@testuser5.ru', 'Mister1', 'Twister1',None)
+new_user.save_to_db()
--- project/database.py
+++ project/database.py
@@ -1,16 +1,43 @@
 # allowing to interact with the DB
 from psycopg2 import pool
 
-connection_pool = pool.SimpleConnectionPool(1,
-                                            10,
-                                            user='postgres',
-                                            password='1234',
-                                            database='Learning',
-                                            host='localhost'
-                                            )
-# if no connections available, then it is gonna create 1
-# mincon = how many connections should be created once the pool is created. here 1
-# maxcon = max number of connections. here 10
+class Database:
+    # If put here the connection won't be created immediately when the app is run
+    # Before it was the first thing to be created
+        __connection_pool = None
+        # this is the property of the class itself
+        # it is not in the init method - therefore it belongs to the class, not to the objects of the class!
+        # It is shared among all Database objects.
+
+        # NB! __ - this is the way to make this variable kinda private (like in C# private)
+
+        #@staticmethod  # would be the same as @classmethod and initialize(cls) and cls.connection_pool = ...
+        # Need this for app.py: Database.initialize() and print(Database.connection_pool)
+        @classmethod
+        def initialize(cls, **kwargs): # not __init__ method! does not executed automatically
+            cls.__connection_pool = pool.SimpleConnectionPool(1,
+                                                              10,
+                                                              **kwargs
+                                                              )
+            # if no connections available, then it is gonna create 1
+            # mincon = how many connections should be created once the pool is created. here 1
+            # maxcon = max number of connections. here 10
+
+            # **kwargs - means any named parameters. any number of them
+
+        @classmethod
+        def get_connection(cls):
+            return cls.__connection_pool.getconn()
+
+        @classmethod
+        def return_connection(cls, connection):
+            # in this case we need the reference to connection that we want to put back in
+            Database.__connection_pool.putconn(connection)
+
+
+        @classmethod
+        def close_all_connections(cls):
+            Database.__connection_pool.closeall()
 
 # this class is to teach the usage of WITH statement
 
@@ -25,8 +52,8 @@ class CursorFromConnectionPool:
     # init method is called before enter method
     def __enter__(self):
         # enter method is the beginning of the with statement
-        self.connection = connection_pool.getconn()
-        self.cursor = self.connection.cursor
+        self.connection = Database.get_connection()
+        self.cursor = self.connection.cursor()
         return self.cursor
         # thus this connection from the pool goes into connection variable in user.py when called using with statement
     # gets and returns a new connection. Now this works in user.py 'with ConnectionPool() as connection:'
@@ -42,7 +69,7 @@ class CursorFromConnectionPool:
             self.cursor.close()
             self.connection.commit() # DON'T forget! Commit then close
             # here we should be returning the connections
-            connection_pool.putconn(self.connection)
+            Database.return_connection(self.connection)
             # putting the connection back to the pool
 # access
 # cp = ConnectionPool()
